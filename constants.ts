528900d684951954f75ac5e179d7b7884fbc529e
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2023-07-02 13:42:47 -0300

    fix: GitHub repo URL

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 export const IS_PROD = process.env.NODE_ENV === "production";
-export const GITHUB_REPO = "https://faunaplayground.vercel.app";
+export const GITHUB_REPO = "https://github.com/BrunoQuaresma/fauna-playground";
 export const BRUNO_TWITTER = "https://twitter.com/bruno__quaresma";
